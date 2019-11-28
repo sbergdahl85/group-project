45edd5361f466131bd5840adda1fe0a15066c239
Simon <s171927@student.hb.se>
2019-11-28 15:59:31 +0100

    Updates

---
+++ precache-manifest.375c0e5f38f9cfa5297a609890da8029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2c7a8852ae6d1365277303e460687d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "556f683ee61257c10cf9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "61fb15dfa3cb133b4ac7",
+    "url": "/static/js/2.20b04958.chunk.js"
+  },
+  {
+    "revision": "556f683ee61257c10cf9",
+    "url": "/static/js/main.19848bd6.chunk.js"
+  },
+  {
+    "revision": "56a7db33578a608ef258",
+    "url": "/static/js/runtime-main.c2a4ebfe.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
